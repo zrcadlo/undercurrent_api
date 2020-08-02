48fc7125506ba9f051b1180f6879e31fe0e2dbc5
Luis Borjas Reyes <luisfborjas@gmail.com>
2020-08-02 16:13:52 -0400

    Introduce dream sample ids function, with the same filters as search

--- backend/src/Models.hs
+++ backend/src/Models.hs
@@ -44,7 +44,8 @@ import Util (zeroTime)
 import Database.Persist.Sql.Raw.QQ (sqlQQ)
 import Database.Persist.Sql (Single(..))
 import Database.Persist.Postgresql (runSqlConn)
-import Control.Monad.Logger (LoggingT, runStdoutLoggingT, NoLoggingT(runNoLoggingT))
+import Control.Monad.Logger (LoggingT, runStdoutLoggingT)
+import Database.Esqueleto (SqlQuery)
 
 type DBM m a = (MonadIO m) => ReaderT SqlBackend m a
 type QueryM a = forall m. DBM m a
@@ -204,68 +205,96 @@ noDreamFilters = DreamFilters
     Nothing
     Nothing
 
+type OwnerFilters = (Maybe (Key UserAccount, Bool))
+
 filteredDreams
     :: DreamFilters
-    -> (Maybe (Key UserAccount, Bool))
+    -> OwnerFilters
     -> QueryM [(Entity Dream, Entity UserAccount)]
-filteredDreams DreamFilters{..} userConditions = do
-    let maybeNoConditions = maybe (return ())
+filteredDreams dreamFilters userConditions = do
     E.select . E.from $ \(dream `E.InnerJoin` userAccount) -> do
-        -- Ownership filters
         E.on (userAccount E.^. UserAccountId E.==. dream E.^. DreamUserId)
-        maybe
-            -- if no user conditions, simply look through public dreams
-            (E.where_ (dream E.^. DreamIsPrivate E.==. E.val False))
-            -- if there are, check if the given user is the owner!
-            (\(userId, isOwner) ->
-                if isOwner then
-                    E.where_
-                        (     dream
-                        E.^.  DreamUserId
-                        E.==. (E.val userId)
-                        )
-                else
-                    E.where_
-                        (     dream
-                        E.^.  DreamUserId
-                        E.==. (E.val userId)
-                        E.&&. dream
-                        E.^.  DreamIsPrivate
-                        E.==. (E.val False)
-                    )
-            )
-            userConditions
-        -- inspired by: https://gist.github.com/bitemyapp/89c5e0663bddc3b1c78f5e3fa33e7dc4#file-companiescount-hs-L79
-        -- Dream Filters
-        maybeNoConditions (\l -> E.where_ (dream E.^. DreamIsLucid     E.==. E.val l)) filterLucid
-        maybeNoConditions (\n -> E.where_ (dream E.^. DreamIsNightmare E.==. E.val n)) filterNightmare
-        maybeNoConditions (\r -> E.where_ (dream E.^. DreamIsRecurring E.==. E.val r)) filterRecurring
-        maybeNoConditions (\es -> E.where_ (E.just (dream E.^. DreamEmotions) E.@>. (E.jsonbVal es))) filterEmotions
-        -- more info on full text queries:
-        -- https://www.postgresql.org/docs/current/textsearch-tables.html
-        maybeNoConditions
-            (\kw ->
-                E.where_
-                    $ (tsVector $ dream E.^. DreamTitle E.++. (E.val " ") E.++. dream E.^. DreamDescription)
-                    @@.
-                    (webTsQuery $ E.val kw)
-            )
-            filterKeyword
-        -- User Account filters
-        maybeNoConditions (\b -> E.where_ (userAccount E.^. UserAccountLocation E.==. (E.just $ E.val b))) filterLocation
-        maybeNoConditions (\g -> E.where_ (userAccount E.^. UserAccountGender E.==. (E.just $ E.val g))) filterGender
-        maybeNoConditions (\z -> E.where_ (userAccount E.^. UserAccountZodiacSign E.==. (E.just $ E.val z))) filterZodiacSign
-        -- Ranges
-        maybeNoConditions (\before -> E.where_ (dream E.^. DreamDreamedAt E.<=. E.val before)) filterBefore
-        maybeNoConditions (\after -> E.where_ (dream E.^. DreamDreamedAt  E.>=. E.val after)) filterAfter
-        -- keyset pagination
-        maybeNoConditions (\lastSeen -> E.where_(dream E.^. DreamId E.<. E.val lastSeen)) filterLastSeenId
+        restrictByOwnerConditions dream userConditions
+        restrictByDreamFilters dream userAccount dreamFilters
+        -- PAGINATION
         -- default page size is 100, max is 1000.
-        E.limit $ maybe 100 (\l-> if l > 1000 then 1000 else l) filterLimit
+        maybeNoConditions (\lastSeen -> E.where_(dream E.^. DreamId E.<. E.val lastSeen)) (filterLastSeenId dreamFilters)
+        E.limit $ maybe 100 (\l-> if l > 1000 then 1000 else l) (filterLimit dreamFilters)
         E.orderBy [ E.desc (dream E.^. DreamId) ]
         return (dream, userAccount)
 
--- | Documentation helpers
+sampleDreamIds
+    :: Int64
+    -> DreamFilters
+    -> OwnerFilters
+    -> QueryM [E.Value (Key Dream)]
+sampleDreamIds sampleSize dreamFilters userConditions = do
+    E.select . E.from $ \(dream `E.InnerJoin` userAccount) -> do
+        E.on (userAccount E.^. UserAccountId E.==. dream E.^. DreamUserId)
+        restrictByOwnerConditions dream userConditions
+        restrictByDreamFilters dream userAccount dreamFilters
+        E.orderBy [E.rand]
+        -- 1000 is statistically significant up to perhaps 10,000,000 rows,
+        -- with a margin of error of 3% and confidence of 95%
+        -- according to cursory research with tools like:
+        -- https://www.surveymonkey.com/mp/sample-size-calculator/
+        E.limit sampleSize 
+        return (dream E.^. DreamId)
+
+
+maybeNoConditions :: forall a. (a -> SqlQuery ()) -> Maybe a -> SqlQuery ()
+maybeNoConditions = maybe $ pure ()
+
+--restrictByUserConditions :: (Maybe (Key UserAccount, Bool))
+restrictByOwnerConditions :: E.SqlExpr (Entity Dream) -> Maybe (Key UserAccount, Bool) -> SqlQuery ()
+restrictByOwnerConditions dream userConditions =
+    maybe
+    -- if no user conditions, simply look through public dreams
+    (E.where_ (dream E.^. DreamIsPrivate E.==. E.val False))
+    -- if there are, check if the given user is the owner!
+    (\(userId, isOwner) ->
+        if isOwner then
+            E.where_
+                (     dream
+                E.^.  DreamUserId
+                E.==. (E.val userId)
+                )
+        else
+            E.where_
+                (     dream
+                E.^.  DreamUserId
+                E.==. (E.val userId)
+                E.&&. dream
+                E.^.  DreamIsPrivate
+                E.==. (E.val False)
+            )
+    )
+    userConditions
+
+restrictByDreamFilters :: E.SqlExpr (Entity Dream) -> E.SqlExpr (Entity UserAccount) -> DreamFilters -> SqlQuery ()
+restrictByDreamFilters dream userAccount DreamFilters{..} = do
+    -- inspired by: https://gist.github.com/bitemyapp/89c5e0663bddc3b1c78f5e3fa33e7dc4#file-companiescount-hs-L79
+    maybeNoConditions (\l -> E.where_ (dream E.^. DreamIsLucid     E.==. E.val l)) filterLucid
+    maybeNoConditions (\n -> E.where_ (dream E.^. DreamIsNightmare E.==. E.val n)) filterNightmare
+    maybeNoConditions (\r -> E.where_ (dream E.^. DreamIsRecurring E.==. E.val r)) filterRecurring
+    maybeNoConditions (\es -> E.where_ (E.just (dream E.^. DreamEmotions) E.@>. (E.jsonbVal es))) filterEmotions
+    -- more info on full text queries:
+    -- https://www.postgresql.org/docs/current/textsearch-tables.html
+    maybeNoConditions
+        (\kw ->
+            E.where_
+                $ (tsVector $ dream E.^. DreamTitle E.++. (E.val " ") E.++. dream E.^. DreamDescription)
+                @@.
+                (webTsQuery $ E.val kw)
+        )
+        filterKeyword
+    -- User Account filters
+    maybeNoConditions (\b -> E.where_ (userAccount E.^. UserAccountLocation E.==. (E.just $ E.val b))) filterLocation
+    maybeNoConditions (\g -> E.where_ (userAccount E.^. UserAccountGender E.==. (E.just $ E.val g))) filterGender
+    maybeNoConditions (\z -> E.where_ (userAccount E.^. UserAccountZodiacSign E.==. (E.just $ E.val z))) filterZodiacSign
+    -- Ranges
+    maybeNoConditions (\before -> E.where_ (dream E.^. DreamDreamedAt E.<=. E.val before)) filterBefore
+    maybeNoConditions (\after -> E.where_ (dream E.^. DreamDreamedAt  E.>=. E.val after)) filterAfter
 
 -- TODO: the day approaches where it's obvious we need an "application" version of UserAccount.
 sampleUser :: UserAccount
